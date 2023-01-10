54b8ba5b11ad4d20b739c53051a9a1107b6b73ca
Mahendra <mahendrabrowny@gmail.com>
2023-01-10 22:12:34 +0530

    sidebar UI with filter options

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,65 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
-function Sidebar() {
+const Sidebar = ({
+  products,
+  setsortValue,
+  setFilterValues,
+  filterValues,
+  sortValue,
+  setProducts,
+}) => {
   return (
-    <aside className="w-[22%]">
-      <div className="rounded bg-gray-50">Sidebar content goes here</div>
-    </aside>
+    <div className="w-1/5 bg-gray-200 rounded">
+      <span className="flex items-center justify-evenly p-2">
+        <span className="text-lg font-medium">Filters</span>
+        <Button size="sm" variant="outline-danger">
+          Clear all
+        </Button>
+      </span>
+      <hr />
+      <div className="pl-3">
+        <span>Brands</span>
+        {products.map((brand) => (
+          <div key={brand.id}>
+            <input type="checkbox" name={brand.brand} value={brand.brand} />
+            <label className="mx-2">{brand.brand}</label>
+          </div>
+        ))}
+      </div>
+      <div className="sidebar__categories">
+        <span>Sizes</span>
+        {["S", "M", "L", "XL"].map((prod) => (
+          <div
+            key={prod}
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <input type="checkbox" name={prod} value={prod} />
+            <label className="mx-2">{prod}</label>
+          </div>
+        ))}
+      </div>
+      <div className="sidebar__categories">
+        <span>Ideal for</span>
+        {["Men", "Women", "Girl", "Kids"].map((prod) => (
+          <div
+            key={prod}
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <input type="checkbox" name={prod} value={prod} />
+            <label className="mx-2">{prod}</label>
+          </div>
+        ))}
+      </div>
+      <Button variant="outline-primary">Filter</Button>
+    </div>
   );
-}
+};
 
 export default Sidebar;
