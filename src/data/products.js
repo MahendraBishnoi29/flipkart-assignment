70c021261f7de34fc3f2a5ef86c2a936358a65b0
Mahendra <mahendrabrowny@gmail.com>
2023-01-10 16:13:39 +0530

    products array and sort by array

---
+++ src/data/products.js
@@ -0,0 +1,115 @@
+const arr = [
+  {
+    id: 1,
+    name: "Men Sweatshirt",
+    price: 800,
+    size: "M",
+    brand: "Puma",
+    idealFor: "Men",
+    image:
+      "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRqM8EEgHuOh_IoRGVQWVECZteySzs_s874Vi0w1M4ZZ_VJaybAq6OriMzNqIm2fB_qRHE-7SsxjSfl8mAPeFAvCJUfa1xS75Z8ksR-KlADdEVyEF1fIKaT&usqp=CAE",
+  },
+  {
+    id: 2,
+    name: "Puma Women Sweatshirt",
+    price: 700,
+    size: "M",
+    brand: "Puma",
+    idealFor: "Women",
+    image:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTNuyMuuBM1Ziz7iRRvrP8qd6ZA1CJCqztWPJQMU8kD6tddR26A_gDc-YuWuu0sHfPfvDumvXNCtiJMvLoWHfzp6wGw_rUPLo8Df9PG-iqY3h8ktxLP__2r&usqp=CAE",
+  },
+  {
+    id: 3,
+    name: "Puma Track Pant",
+    price: 1100,
+    size: "L",
+    brand: "Puma",
+    idealFor: "Men",
+    image:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRI_VQmP9EYNlKuszl1Axjm5k9sQusvtTA87qhdvpT1b1FGTwpjCke04uKPsohzrdVt7mzPs9-yyoLX6n6R4ULL8016Vr9nBG8KTpU7hGNnscniwAzi9I1w&usqp=CAE",
+  },
+  {
+    id: 4,
+    name: "H&M Sweatshirt",
+    price: 1299,
+    size: "L",
+    brand: "H&M",
+    idealFor: "Men",
+    image:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcR2hqmBYBW-XjTxMDeTtXdh7xrJV3Ld8cYh_r5uxEjamA7HmSs4MFfNqBTmhKYwmLyqe_Xqb_-HkdKaCnVGj5sIpFAGwJw7WlKWVkrr7gmYfR3kRU6FIWdJrQ&usqp=CAE",
+  },
+  {
+    id: 5,
+    name: "H&M Sweatshirt for women",
+    price: 500,
+    size: "S",
+    brand: "H&M",
+    idealFor: "Women",
+    image:
+      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQbBA16EEscp5V-aTdYLMffHMf2J6suc0j9tjpC7UO9BRyhJwwW5vN7Mo_xZeZQvEbh3o-FwwvwxmdSxCStKfUwisxAlTkc5bFfxjgV0qCjrqSu8Wqdi9kY&usqp=CAE",
+  },
+  {
+    id: 6,
+    name: "Adidas Stylish Sweatshirt for women",
+    price: 2199,
+    size: "XL",
+    brand: "Adidas",
+    idealFor: "Women",
+    image:
+      "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSxgFNVzUVG0ZrYcETYmCdTdQvPcqR9hjhe55SGSaccGvjNvMQBizx9hdGrlVyH7odZYijPiHuCH8BPYtil9qWsgLtGvDlg4Gk5prlacrEO&usqp=CAE",
+  },
+  {
+    id: 7,
+    name: "Adidas Essential Hoodie Men",
+    price: 4193,
+    size: "L",
+    brand: "Adidas",
+    idealFor: "Men",
+    image:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSaPRJjC_z_0Iuyx8TmEpnir4BvnmK3GCGUlEm1v7HhPF36qxJO3DvdMapjloW0VPYcLT6tsCupBo1QgZwmhYZzPPMlNsYT_oeobNjL8p8&usqp=CAE",
+  },
+  {
+    id: 8,
+    name: "Gucci Sweater men",
+    price: 5000,
+    size: "M",
+    brand: "Gucci",
+    idealFor: "Men",
+    image:
+      "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQQNL9RH9aMWDbxzgmoooEgJ8GM89h2Ax4Sd5mGqB0GdrstW3mGQppnd3F-97H03QDiJbKv2_EW53c5Sh_hxmgaXWVEL47LiJEL2ULXw3stib8cimzHmcQhsA&usqp=CAE",
+  },
+  {
+    id: 9,
+    name: "Kids Warm Sweatshirt",
+    price: 100,
+    size: "XS",
+    brand: "Decethlon",
+    idealFor: "Kids",
+    image:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQX-QPCmiA_DjQfp7AwOk4-yum1LH4B4jCu9McfCaO63nGKFr5tqvKgVetE51Z_xzrs6PtRFcDmH7QZ8li0XJJYsHtbo92YYRtNeK7GkvwvB5IeMDgsiYfj&usqp=CAE",
+  },
+  {
+    id: 10,
+    name: "Printed Sweatshirt for kids",
+    price: 479,
+    size: "S",
+    brand: "AJIO",
+    idealFor: "Kids",
+    image:
+      "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQxo4nqeO-q9o9CAiED0IMGMY9jF18t-kxU4IrVADX4tr4eCnl1HeyGdd6OgHcBUlQ0EyXnKehXmF2jNGdUtc8nKfZR6MrFtIT7BnLdbW5Fe7ynFbDUFCh6c-A&usqp=CAE",
+  },
+];
+
+export const sortBy = [
+  {
+    id: 1,
+    name: "Price -- Low to High",
+    type: -1,
+  },
+  {
+    id: 2,
+    name: "Price -- High to Low",
+    type: 1,
+  },
+];
