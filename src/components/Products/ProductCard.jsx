3b5cafaa90299f31ca354fa4a4313d7401f38b01
Mahendra <mahendrabrowny@gmail.com>
2023-01-10 22:12:46 +0530

    products card

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,33 +1,40 @@
 import React from "react";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ products }) => {
   return (
-    <div className="">
-      <img src={product.image} className="singlecard__img" alt={product.name} />
-      <div className="singlecard__data">
-        <span className="singlecard__brand">{product.brand}</span>
-        <span className="singlecard__name">
-          {product.name.length < 23 ? (
-            product.name
-          ) : (
-            <span>
-              {product.name.substring(0, 23)}
-              ...
+    <div className="w-3/4 flex">
+      {products?.map((product) => (
+        <div
+          className="container mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl"
+          key={product.id}
+        >
+          <img
+            src={product.image}
+            className="singlecard__img"
+            alt={product.name}
+          />{" "}
+          <div className="singlecard__data">
+            <span className="singlecard__brand">{product.brand}</span>
+            <span className="singlecard__name">
+              {product.name?.length < 23 ? (
+                product.name
+              ) : (
+                <span>
+                  {product.name?.substring(0, 23)}
+                  ...
+                </span>
+              )}
             </span>
-          )}
-        </span>
-        <span className="singlecard__data--price">
-          <span style={{ fontWeight: 500 }}>₹{product.price}</span>{" "}
-          <strike className="singlecard__brand">₹{product.price}</strike>{" "}
-          <span style={{ fontWeight: 500, color: "green" }}>
-            {product.offer} off
-          </span>
-        </span>
-        <div>
-          <span className="singlecard__brand">SIZE: </span>
-          {product.size.map((m) => `${m}, `)}
+            <span className="singlecard__data--price">
+              <span style={{ fontWeight: 500 }}>₹{product?.price}</span>{" "}
+            </span>
+            <div>
+              <span className="singlecard__brand">SIZE: </span>
+              {product.size.map((m) => `${m}, `)}
+            </div>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
