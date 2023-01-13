29779b4863ee72222cd573c7697ddbff5cad368c
Mahendra <mahendrabrowny@gmail.com>
2023-01-13 15:53:05 +0530

    add filter logic and filter states

--- src/store/products.jsx
+++ src/store/products.jsx
@@ -6,7 +6,32 @@ const ProductsContext = createContext([]);
 export const useProducts = () => useContext(ProductsContext);
 
 const ProductsProvider = ({ children }) => {
-  return <ProductsContext.Provider>{children}</ProductsContext.Provider>;
+  const [activeBrands, setActiveBrands] = useState([]);
+  const [activeSizes, setActiveSizes] = useState([]);
+  const [activeIdeals, setActiveIdeals] = useState([]);
+
+  let filteredProducts = products.filter((p) => {
+    const isBrandIn =
+      activeBrands.length <= 0 ? true : activeBrands.includes(p.brand);
+    const isSizesIn =
+      activeSizes.length <= 0 ? true : activeSizes.includes(p.size);
+    const isIdealIn =
+      activeIdeals.length <= 0 ? true : activeIdeals.includes(p.idealFor);
+    return isBrandIn && isSizesIn && isIdealIn;
+  });
+
+  return (
+    <ProductsContext.Provider
+      value={{
+        setActiveBrands,
+        setActiveIdeals,
+        setActiveSizes,
+        products: filteredProducts,
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
 };
 
 export default ProductsProvider;
