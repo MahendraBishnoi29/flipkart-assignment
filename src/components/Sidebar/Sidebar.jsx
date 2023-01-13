255f704f005f2f108a523e868cc6ea42afc404c1
Mahendra <mahendrabrowny@gmail.com>
2023-01-13 15:54:37 +0530

    manage states for filters

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
+import { products as originalProducts } from "../../data/products";
+import { useProducts } from "../../store/products";
 
-const Sidebar = ({ products }) => {
-  const brands = [...new Set(products?.map((p) => p.brand))];
+const Sidebar = () => {
+  const { setActiveBrands, setActiveSizes, setActiveIdeals } = useProducts();
+
+  const handleCheckBoxChecked = (event, callback) => {
+    if (event.target.checked) {
+      callback((prev) => [...prev, event.target.value]);
+    } else {
+      callback((prev) => prev.filter((p) => p !== event.target.value));
+    }
+  };
+
+  const brands = [...new Set(originalProducts?.map((p) => p.brand))];
 
   return (
-    <div className="w-1/5 max-h-[90vh] bg-gray-200 rounded">
+    <div className="w-1/5 max-h-[85vh] bg-gray-200 rounded">
       <span className="flex items-center justify-evenly p-2">
         <span className="text-lg font-medium">Filters</span>
         <Button size="sm" variant="outline-danger">
@@ -17,7 +29,14 @@ const Sidebar = ({ products }) => {
         <span className="font-bold pb-1">Brands</span>
         {brands?.map((brand) => (
           <div key={brand}>
-            <input type="checkbox" name={brand} value={brand} />
+            <input
+              type="checkbox"
+              name={brand}
+              value={brand}
+              onChange={(event) =>
+                handleCheckBoxChecked(event, setActiveBrands)
+              }
+            />
             <label className="mx-2">{brand}</label>
           </div>
         ))}
@@ -32,14 +51,19 @@ const Sidebar = ({ products }) => {
               alignItems: "center",
             }}
           >
-            <input type="checkbox" name={prod} value={prod} />
+            <input
+              type="checkbox"
+              name={prod}
+              value={prod}
+              onChange={(event) => handleCheckBoxChecked(event, setActiveSizes)}
+            />
             <label className="mx-2">{prod}</label>
           </div>
         ))}
       </div>
       <div className="pl-3">
         <span className="font-bold pb-1">Ideal for</span>
-        {["Men", "Women", "Girl", "Kids"].map((prod) => (
+        {["Men", "Women", "Kids"].map((prod) => (
           <div
             key={prod}
             style={{
@@ -47,14 +71,23 @@ const Sidebar = ({ products }) => {
               alignItems: "center",
             }}
           >
-            <input type="checkbox" name={prod} value={prod} />
+            <input
+              type="checkbox"
+              name={prod}
+              value={prod}
+              onChange={(event) =>
+                handleCheckBoxChecked(event, setActiveIdeals)
+              }
+            />
             <label className="mx-2">{prod}</label>
           </div>
         ))}
+
+        <div className="pl-3">
+          <span className="font-bold pb-1">Price High To Low</span>
+          <input type="checkbox" />
+        </div>
       </div>
-      <Button className="m-3" variant="outline-primary">
-        Filter
-      </Button>
     </div>
   );
 };
