fb0d42cbace0a6cbba1f5b49b4a250f25ab059a9
Mahendra <mahendrabrowny@gmail.com>
2023-02-03 11:49:58 +0530

    update state logic

--- src/store/products.jsx
+++ src/store/products.jsx
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from "react";
-import { products } from "../data/products";
 
 const ProductsContext = createContext([]);
 
@@ -11,38 +10,17 @@ const ProductsProvider = ({ children }) => {
   const [activeIdeals, setActiveIdeals] = useState([]);
   const [sortMethod, setSortMethod] = useState("");
 
-  let filteredProducts = products.filter((p) => {
-    const isBrandIn =
-      activeBrands.length <= 0 ? true : activeBrands.includes(p.brand);
-    const isSizesIn =
-      activeSizes.length <= 0 ? true : activeSizes.includes(p.size);
-    const isIdealIn =
-      activeIdeals.length <= 0 ? true : activeIdeals.includes(p.idealFor);
-    return isBrandIn && isSizesIn && isIdealIn;
-  });
-
-  if (sortMethod === "price-high-to-low") {
-    filteredProducts.sort((a, b) => b.price - a.price);
-  } else if (sortMethod === "price-low-to-high") {
-    filteredProducts.sort((a, b) => a.price - b.price);
-  }
-
-  const clearAllFilters = () => {
-    setActiveBrands([]);
-    setActiveSizes([]);
-    setActiveIdeals([]);
-    setSortMethod("");
-  };
-
   return (
     <ProductsContext.Provider
       value={{
+        activeBrands,
         setActiveBrands,
-        setActiveIdeals,
+        activeSizes,
         setActiveSizes,
+        activeIdeals,
+        setActiveIdeals,
+        sortMethod,
         setSortMethod,
-        clearAllFilters,
-        products: filteredProducts,
       }}
     >
       {children}
